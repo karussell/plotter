c3fe26d1c5a12be9bd4f55ea0944904d9d611fc0
Peter <graphhopper@gmx.de>
2022-11-29 23:16:46 +0100

    minor

--- src/main/java/de/genvlin/gui/plot/Demo.java
+++ src/main/java/de/genvlin/gui/plot/Demo.java
@@ -5,30 +5,27 @@ import de.genvlin.core.data.VectorInterface;
 import de.genvlin.core.data.XYVectorInterface;
 
 import javax.swing.*;
-import java.text.ParseException;
 
 public class Demo {
 
-    public static void main(String[] args) throws ParseException {
-LivePlotPanel panel = new LivePlotPanel();
-SwingUtilities.invokeLater(() -> {
-    int frameHeight = 800;
-    int frameWidth = 1200;
-    JFrame frame = new JFrame("Plotter UI - Fast&Ugly");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(frameWidth, frameHeight);
-    frame.add(panel.getComponent());
-    frame.setVisible(true);
+    public static void main(String[] args) {
+        LivePlotPanel panel = new LivePlotPanel();
+        MainPool pool = MainPool.getDefault();
+        XYVectorInterface data = pool.createXYVector(VectorInterface.class, VectorInterface.class);
+        for (double i = 0; i < 10; i += .1) {
+            data.add(i, Math.sin(i));
+        }
+        panel.getPlotPanel().addData(data);
+        panel.getPlotPanel().automaticScaleAll();
 
-    MainPool pool = MainPool.getDefault();
-    XYVectorInterface data = pool.createXYVector(VectorInterface.class, VectorInterface.class);
-    for (double i = 0; i < 10; i += .1) {
-        data.add(i, Math.sin(i));
-    }
-    panel.getPlotPanel().addData(data);
-    panel.setEnabled(true);
-    panel.getPlotPanel().automaticScaleAll();
-    panel.getPlotPanel().repaint();
-});
+        SwingUtilities.invokeLater(() -> {
+            int frameHeight = 800;
+            int frameWidth = 1200;
+            JFrame frame = new JFrame("Plotter UI - Fast&Ugly");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(frameWidth, frameHeight);
+            frame.add(panel.getComponent());
+            frame.setVisible(true);
+        });
     }
 }
